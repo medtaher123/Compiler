0d0ff5d9e113d92d0abf030d6cf2464dd046a6e0
medtaher <medtaherbh@gmail.com>
2026-04-23 00:03:30 +0100

    feat: string token

---
+++ tokens/string_token.py
@@ -0,0 +1,11 @@
+from tokens.token import Token
+
+
+class StringToken(Token):
+    PATTERN = r'"(?:[^"\\]|\\.)*"'
+
+    def __repr__(self):
+        return f'STR({self.value})'
+
+    def get_type_name(self):
+        return "<str>"
